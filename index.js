f27a9219a9acad3e97b84fd159b3907b99b98fd4
Mark Greenwood <mark.greenwood@cascadeenergy.com>
2018-06-27 11:16:41 -0700

    Added comments about reindexing configuration

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+/*
+version_type: 'external'
+Preserves version from source, create missing docs, update docs whose destination
+version is older than source. Setting this to 'internal' (or default) will blindly
+overwrite all docs in the destination index.
+
+op_type: 'create'
+This will create missing docs in the destination index but all existing docs will create
+a version conflict.
+
+conflicts: 'proceed'
+By default, conflicts abort the reindex, but conflicts: 'proceed' will just count them
+and continue with other docs.
+
+You can add a query to the source to only copy a subset of the docs from the source.
+*/
+
 const reindexSpec = (src, dest) => ({
   body: {
     source: {
--- startdb.sh
+++ startdb.sh
@@ -3,7 +3,7 @@ curl --fail -s -XGET localhost:9200
 if [ 7 -eq $? ]
 then
     echo 'Starting elasticsearch...'
-    nohup ~/elasticsearch-6.2.2/bin/elasticsearch &
+    ~/elasticsearch-6.2.2/bin/elasticsearch &
 else
     echo 'Everything is OK'
 fi
