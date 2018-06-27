66bbb22fba8ba309dc39c1abf73f3aa6e9c8e532
Mark Greenwood <mark.greenwood@cascadeenergy.com>
2018-06-27 11:41:47 -0700

    startdb.sh

--- startdb.sh
+++ startdb.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-curl --fail -s -XGET localhost:9200
+RESPONSE=$(curl --fail -s -XGET localhost:9200)
 if [ 7 -eq $? ]
 then
     echo 'Starting elasticsearch...'
-    ~/elasticsearch-6.2.2/bin/elasticsearch &
+    ~/elasticsearch-6.2.2/bin/elasticsearch > ~/elasticsearch.log 2>&1 &
+    until
+        curl --silent --fail -XGET localhost:9200
+    do
+        printf '.'
+        sleep 1
+    done
 else
-    echo 'Everything is OK'
+    echo $RESPONSE
 fi
